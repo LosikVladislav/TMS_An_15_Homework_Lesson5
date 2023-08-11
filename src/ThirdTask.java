95fe42b625b554cf058defa94429540ac972f2cb
losikvladislav <vlad.losik@yandex.ru>
2023-08-11 23:38:15 +0300

    completed third assignment

---
+++ src/ThirdTask.java
@@ -0,0 +1,29 @@
+/**
+ * Создайте двумерный массив целых чисел.
+ * Выведите на консоль сумму всех элементов массива
+ */
+import java.util.Arrays;
+import java.util.Scanner;
+public class ThirdTask {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter row number");
+        int row = scanner.nextInt();
+        System.out.println("Enter colum number");
+        int colum = scanner.nextInt();
+        int [][] array = new int[row][colum];
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array[i].length; j++){
+                array[i][j] = (int)(Math.random()*10);
+            }
+        }
+        System.out.println(Arrays.deepToString(array));
+        int sum = 0;
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array[i].length; j++){
+                sum +=array[i][j];
+            }
+        }
+        System.out.println("sum of all arrays elements " + sum);
+    }
+}
